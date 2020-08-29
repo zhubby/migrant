60bd582f055436431ed30524d582d699a33b0869
超人力霸王战士 <9190779+zhubby@users.noreply.github.com>
2020-08-30 00:32:09 +0800

    Initial Commit

---
+++ migrant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  migrant
+//
+//  Created by 朱彦 on 2020/8/30.
+//  Copyright © 2020 zhubby. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
